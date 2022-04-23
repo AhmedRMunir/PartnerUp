14128f5244a577f9046a5fa1589b881608483cdb
James McGivern <jamesmcgivern8@gmail.com>
2022-04-23 15:14:52 -0700

    init firebase and firestore

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from "firebase/firestore";
+import { collection, query, where, getDocs, initializeFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCN1E0yCkUbk2v8kQH3PGPpvAqICZkrHOc",
+  authDomain: "partnerup-8fb5c.firebaseapp.com",
+  projectId: "partnerup-8fb5c",
+  storageBucket: "partnerup-8fb5c.appspot.com",
+  messagingSenderId: "757257283487",
+  appId: "1:757257283487:web:0c6e02b206ade0c9033faa"
+};
+
+// Initialize Firebase
+const firebaseapp = initializeApp(firebaseConfig);
+
+const db = getFirestore(firebaseapp);
 
 function App() {
+  getPreferences();
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +40,16 @@ function App() {
       </header>
     </div>
   );
+}
+
+async function getPreferences() {
+  const q = query(collection(db, "preferences"));
+
+  const querySnapshot = await getDocs(q);
+  querySnapshot.forEach((doc) => {
+    // doc.data() is never undefined for query doc snapshots
+    console.log(doc.id, " => ", doc.data());
+  });
 }
 
 export default App;
