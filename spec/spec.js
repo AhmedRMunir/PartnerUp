1abade62597703d824e52f15b6c7e68ffdc756a9
Detlef Knauss <detlefk@hotmail.com>
2022-05-10 13:19:03 -0700

    Switched testing to jasmine

---
+++ spec/spec.js
@@ -0,0 +1,6 @@
+//import { FormCreator } from '../src/pages/FormCreator.js';
+describe("MyJSUtilities", function() { 
+    it("should be able to lower case a string",function() {
+        expect().nothing();
+    });
+});
