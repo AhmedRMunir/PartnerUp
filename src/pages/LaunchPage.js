fcd6d2a013bca559c164becb6aa9c99fd8397b85
achoat <achoat@cs.washington.edu>
2022-05-27 19:10:35 -0700

    bare-bone implemntation of google SSO, needs debugging

--- src/pages/LaunchPage.js
+++ src/pages/LaunchPage.js
@@ -1,14 +1,32 @@
-import { Link } from 'react-router-dom';
+import GoogleLogin from 'react-google-login';
+import { useNavigate } from "react-router-dom";
+import InstructorHome from "./InstructorHome.js";
+
 
 function LaunchPage() {
+  let navigate = useNavigate();
+  const handleLogin = (googleData) => {
+    console.log("test");
+    console.log(googleData);
+    let path = '/instructor-home';
+    navigate(path);
+  };
+  const handleFailure = (result) => {
+    console.log(result);
+  };
   return (
     <div>
          <h1>PartnerUp!</h1>
-         <Link to="instructor-home">
-            <button>Login with SSO</button>
-        </Link>
+         <GoogleLogin
+            clientId={'757257283487-66gfaadgquqni05m3avlc0jmkdt41rio.apps.googleusercontent.com'}
+            buttonText ="Log in with Google SSO"
+            onSuccess={handleLogin}
+            onFailure={handleFailure}
+            cookiePolicy={'single_host_origin'}
+            ></GoogleLogin>
     </div>
   )
 }
+
 
 export default LaunchPage
