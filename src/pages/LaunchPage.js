cb0fec05fbcb8f966c84ba4b673f330b224265d3
jamesm62 <74880285+jamesm62@users.noreply.github.com>
2022-05-28 04:19:46 -0700

    Finished Google SSO login (#14)
    Co-authored-by: James McGivern <jamesmcgivern8@gmail.com>

--- src/pages/LaunchPage.js
+++ src/pages/LaunchPage.js
@@ -1,12 +1,53 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { getAuth, signInWithRedirect, getRedirectResult, GoogleAuthProvider } from "firebase/auth";
+import React, { useEffect } from 'react';
+
+function login(auth) {
+  const provider = new GoogleAuthProvider();
+  signInWithRedirect(auth, provider);
+}
 
 function LaunchPage() {
+  const auth = getAuth();
+  let navigate = useNavigate();
+  useEffect(() => {
+    getRedirectResult(auth)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access Google APIs.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+
+      // The signed-in user info.
+      const user = result.user;
+      if (user == null) {
+        return;
+      }
+      if (user.email.endsWith("uw.edu") || user.email.endsWith("cs.washington.edu")) {
+        navigate('/instructor-home');
+      } else {
+        alert('Sorry, you must sign in with a UW email account to access this service!');
+      }
+    }).catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      if (error.user == null) {
+        return;
+      }
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      if (credential.user == null) {
+        return;
+      }
+      // ...
+    });
+  });
   return (
     <div>
-         <h1>PartnerUp!</h1>
-         <Link to="instructor-home">
-            <button>Login with SSO</button>
-        </Link>
+      <h1>PartnerUp!</h1>
+      <button onClick={() => login(auth)}>Login with SSO</button>
     </div>
   )
 }
